c7585f21baf3ddc5be99853ac58a80acb6b14af1
Wambui-123 <yvonne.muthui@student.moringaschool.com>
2022-04-11 18:40:12 +0300

    created new component

--- src/app/highlight.directive.spec.ts
+++ src/app/highlight.directive.spec.ts
@@ -1,8 +1,33 @@
-import { HighlightDirective } from './highlight.directive';
+// import { HighlightDirective } from './highlight.directive';
 
-describe('HighlightDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighlightDirective();
-    expect(directive).toBeTruthy();
-  });
-});
+// describe('HighlightDirective', () => {
+//   it('should create an instance', () => {
+//     const directive = new HighlightDirective();
+//     expect(directive).toBeTruthy();
+//   });
+// });
+
+
+
+  
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlightQoute]'
+})
+export class HighlightQouteDirective {
+
+  constructor(private elem:ElementRef) { 
+  }
+  @HostListener("click") onClicks(){
+    this.textDeco("green")
+  }
+
+  @HostListener("dblclick") onDoubleClicks(){
+    this.textDeco("None")
+  }
+  private textDeco(action:string){
+    this.elem.nativeElement.style.color=action;
+  }
+
+}
